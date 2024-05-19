be7f7f49ec21a9065e23c8facaee0de8ee4bec6b
Nwite Victor Ugochukwu <nwitevictor7@gmail.com>
2024-05-19 22:23:05 +0100

    Add files via upload

---
+++ utils/database.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+export const connectToDB = async() =>{
+    mongoose.set("strictQuery", true);
+
+    if(isConnected){
+        console.log("MongoDB is already connected");
+        return;
+    }
+    try{
+        await mongoose.connect(process.env.MongoDB_URL);
+        isConnected = true;
+        console.log("Connected to MongoDB");
+    }catch(error){
+        console.log("MongoDb not connected", error);
+    }
+}
