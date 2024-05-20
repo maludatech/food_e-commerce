22bfb7334140309540f49ffa43f28162616e3725
Nwite Victor Ugochukwu <nwitevictor7@gmail.com>
2024-05-20 19:17:19 +0100

    Update route.js

--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -7,33 +7,40 @@ export const GET = async (req) => {
         await connectToDB();
         const blogs = await Blog.find();
         if (!blogs.length) {
-            return new Response(JSON.stringify({ message: "Blogs not found" }), { status: 404 });
+            return new Response(JSON.stringify({ message: "Blogs not found" }), { 
+                status: 404, 
+                headers: { 'Content-Type': 'application/json' }
+            });
         }
-        return new Response(JSON.stringify(blogs), { status: 200 });
+        return new Response(JSON.stringify(blogs), { 
+            status: 200, 
+            headers: { 'Content-Type': 'application/json' }
+        });
     } catch (error) {
         console.error("Error fetching blogs", error);
-        return new Response(JSON.stringify({ message: "Error fetching blogs" }), { status: 500 });
+        return new Response(JSON.stringify({ message: "Error fetching blogs" }), { 
+            status: 500, 
+            headers: { 'Content-Type': 'application/json' }
+        });
     }
 };
 
-// POST request handler
-export const POST = async (req) => {
-    const { title, imageUrl, content } = await req.json();
-    try {
-        await connectToDB();
-        const blog = new Blog({
-            title: title, 
-            imageUrl: imageUrl, 
-            content: content
-        });
-        await blog.save();
 
-        if (!blog) {
-            return new Response(JSON.stringify({ message: "Failed to create blog" }), { status: 404 });
-        }
-        return new Response(JSON.stringify({ message: "Blog created successfully" }), { status: 200 });
+export const POST = async (req) => {
+    try {
+        const { title, imageUrl, content } = await req.json();
+        await connectToDB();
+        const blog = new Blog({ title, imageUrl, content });
+        await blog.save();
+        return new Response(JSON.stringify({ message: "Blog created successfully" }), { 
+            status: 200, 
+            headers: { 'Content-Type': 'application/json' }
+        });
     } catch (error) {
         console.error("Error creating blog", error);
-        return new Response(JSON.stringify({ message: "Error creating blog" }), { status: 500 });
+        return new Response(JSON.stringify({ message: "Error creating blog" }), { 
+            status: 500, 
+            headers: { 'Content-Type': 'application/json' }
+        });
     }
 };
